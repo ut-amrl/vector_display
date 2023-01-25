0e61581e52c21649871a1f5d70265249c685dfdb
Zayne Sprague <zayne_sprague@yahoo.com>
2023-01-25 16:08:55 -0600

    Dockerization completed

--- docker/set_paths.sh
+++ docker/set_paths.sh
@@ -3,8 +3,9 @@ source /opt/ros/noetic/setup.bash
 
 DIR=$(pwd)
 echo $DIR
-export ROS_PACKAGE_PATH=/home/rosdev/src/amrl_msgs:$ROS_PACKAGE_PATH
+export ROS_PACKAGE_PATH=/home/rosdev/vector_display/src/amrl_msgs:$ROS_PACKAGE_PATH
 
-export ROS_PACKAGE_PATH=/home/rosdev/src/:$ROS_PACKAGE_PATH
+export ROS_PACKAGE_PATH=/home/rosdev/vector_display:$ROS_PACKAGE_PATH
 
-export ROS_MASTER_URI=http://rosmaster:11311
+export ROS_MASTER_URI=http://vector_display_rosmaster:11311
+export DISPLAY=unix:0
